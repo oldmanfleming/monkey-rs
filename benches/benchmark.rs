4aa0eda43f00b54d0a1a8f3df6b1d95318f55d73
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-01-29 14:55:58 -0800

    Add benchmark for fibonacci

---
+++ benches/benchmark.rs
@@ -0,0 +1,37 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use monkey_rs::{new_compiler, new_interpreter};
+
+const INPUT: &str = r#"
+let fibonacci = fn(x) {
+  if (x == 0) {
+    0
+  } else {
+    if (x == 1) {
+      return 1;
+    } else {
+      fibonacci(x - 1) + fibonacci(x - 2);
+    }
+  }
+};
+
+fibonacci(10);
+"#;
+
+fn fib_benchmark(c: &mut Criterion) {
+    c.bench_function("interpteter", |b| {
+        b.iter(|| {
+            let mut engine = new_interpreter();
+            engine.run(&black_box(INPUT)).unwrap();
+        })
+    });
+
+    c.bench_function("compiler", |b| {
+      b.iter(|| {
+          let mut engine = new_compiler();
+          engine.run(&black_box(INPUT)).unwrap();
+      })
+    });
+}
+
+criterion_group!(benches, fib_benchmark);
+criterion_main!(benches);
